d0965d56bdf2ffb61c6c1b60700064e8af10aaac
Dao Ngoc <ngoc@Dao-Ngocs-Mac-mini.local>
2010-10-13 19:38:12 +0900

    Update to match the new structure of Xitrum

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -12,7 +12,8 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
 
   override def libraryDependencies =
     Set(
-      "cntt" %% "xitrum" % "0.1-SNAPSHOT" % "compile"
+      "cntt"       %% "xitrum"     % "0.1-SNAPSHOT"  % "compile->default",
+      "postgresql" %  "postgresql" % "8.4-701.jdbc4" % "compile->default"
     ) ++ super.libraryDependencies
 
   override def mainClass = Some("colinh.Http")
