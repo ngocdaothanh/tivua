c517cb56898c2684bc96062480c9e44723c473e7
Ngoc Dao <ngoc@ngoc-2.local>
2010-10-24 16:51:57 +0900

    Clean up Project.scala

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -7,15 +7,14 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val localIvyRepo = "Local Ivy" at
     "file://" + Path.userHome + "/.ivy2/local"
 
-  val scalateRepo = "Scalate Repo" at
+  val scalateRepo = "Scalate" at
     "http://repo.fusesource.com/nexus/content/repositories/snapshots"
 
-  override def libraryDependencies =
-    Set(
-      "cntt"           %% "xitrum"          % "0.1-SNAPSHOT"  % "compile",
-      "ch.qos.logback" %  "logback-classic" % "0.9.25",
-      "postgresql"     %  "postgresql"      % "8.4-701.jdbc4" % "compile"
-    ) ++ super.libraryDependencies
+  override def libraryDependencies = Set(
+    "cntt"           %% "xitrum"          % "0.1-SNAPSHOT",
+    "ch.qos.logback" %  "logback-classic" % "0.9.25",
+    "postgresql"     %  "postgresql"      % "8.4-701.jdbc4"
+  ) ++ super.libraryDependencies
 
   override def mainClass = Some("colinh.Boot")
 }
